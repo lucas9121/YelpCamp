8d0732533896c8d9a248572b4afafc9ed7db4e61
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-15 14:09:16 -0400

    updating app

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = 3000
 const path = require('path')
+const mongoose = require('mongoose')
+const PORT = process.env.port || 3000
+const Campground = require('./models/campground')
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+const db = mongoose.connection;
+//if error, then show error
+db.on("error", console.error.bind(console, "connection error:"));
+//otherwise console log connected
+db.once("open", () => {
+    console.log("Database connected")
+})
 
 
 app.set('view engine', 'ejs')
@@ -10,6 +27,12 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping.'})
+    await camp.save()
+    res.send(camp)
 })
 
 app.listen(PORT, () => {
