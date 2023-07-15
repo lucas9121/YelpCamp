62bdb5c012600fcf31fa81cebb08a746fda6a6c9
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-15 13:48:02 -0400

    updating app

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const path = require('path')
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(PORT, () => {
     console.log(`serving on port ${PORT}`)
-})
-
-app.get('/', (req, res) => {
-    res.send('HELLO WORLD!')
 })
