4f77353be1c066ae82ad46e69bf9651076dec3ab
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-16 12:18:44 -0400

    updating app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const PORT = process.env.port || 3000
 const Campground = require('./models/campground')
 
@@ -26,6 +27,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 //parses req.body in the post request
 app.use(express.urlencoded({extended: true}))
+// tricks form into thinking other requets are post request
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
@@ -47,11 +50,27 @@ app.post('/campgrounds', async (req, res) => {
     res.redirect(`/campground/${newCampground._id}`)
 })
 
-app.get('/campground/:id', async (req, res) => {
+
+app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     // console.log(campground)
     res.render('campgrounds/show', {campground})
 })
+
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground})
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params
+    const campgground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true})
+    res.redirect(`/campgrounds/${campgground._id}`)
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`serving on port ${PORT}`)
