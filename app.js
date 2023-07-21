af58c4c8e1d45259ea8280901fc5241291ca3f6c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-21 15:51:15 -0400

    adding review server side validation

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
-const {campgroundSchema} = require('./schemas')
+const {campgroundSchema, reviewSchema} = require('./schemas')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
@@ -45,6 +45,16 @@ app.use(methodOverride('_method'))
 const validateCampground = (req, res, next) => {
     // server side validation
     const {error} = campgroundSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body)
     if(error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
@@ -102,7 +112,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
 }))
 
 // Reviews
-app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     const review = new Review(req.body.review)
     campground.reviews.push(review)
