5e5946aafdb005de5e85773d77e555750784ef6e
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-21 15:08:28 -0400

    updating app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
-const Joi = require('joi')
+const {campgroundSchema} = require('./schemas')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
@@ -30,12 +30,30 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+
+
+
+////////// Middleware ///////////////
 //parses req.body in the post request
 app.use(express.urlencoded({extended: true}))
+
 // tricks form into thinking other requets are post request
 app.use(methodOverride('_method'))
 
+// joi validation
+const validateCampground = (req, res, next) => {
+    // server side validation
+    const {error} = campgroundSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
 
+
+/////////////// CRUD operations ////////////////
 app.get('/', (req, res) => {
     res.render('home', {what: 'Home'})
 })
@@ -49,24 +67,8 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new', {what: "Add Campground"})
 })
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campgground) throw new ExpressError('Invalid Campground Data', 400)
-    // server side validation
-    //https://joi.dev/api/?v=17.9.1
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            descrition: Joi.string().required()
-        }).required()
-    })
-    const {error} = campgroundSchema.validate(req.body)
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
     const newCampground = new Campground(req.body.campground)
     await newCampground.save()
     res.redirect(`/campgrounds/${newCampground._id}`)
@@ -84,7 +86,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req, res, next) => {
     res.render('campgrounds/edit', {what: `Update ${campground.title}`, campground})
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res, next) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const {id} = req.params
     // "run validators " update the document according to the schema rules.
     // "new" returns the updated document, rather than the original
@@ -104,7 +106,7 @@ app.all('*', (req, res, next) => {
 })
 
 
-// Error Handler
+//////////////// Error Handler ////////////////////
 app.use((err, req, res, next) => {
     const {statusCode = 500} = err;
     if(!err.message) err.message = 'Oh No! Something went wrong.'
