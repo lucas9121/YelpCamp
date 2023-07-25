1c3d639d45fc16c88cbe0ac423e5eb1a13c356a5
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-25 13:18:50 -0400

    adding flash error messages

--- routes/campground.js
+++ routes/campground.js
@@ -42,11 +42,19 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews')
+    if(!campground){ 
+        req.flash('error', 'Campground not found!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', {campground, what: campground.title})
 }))
 
 router.get('/:id/edit', catchAsync(async(req, res, next) => {
     const campground = await Campground.findById(req.params.id)
+    if(!campground){ 
+        req.flash('error', 'Campground not found!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/edit', {what: `Update ${campground.title}`, campground})
 }))
 
