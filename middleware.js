9680189ca99b6fe4c436598c7d16a9371a7b28f8
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-28 02:33:06 -0400

    adding return to middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in.');
         return res.redirect('/login');
     }
     next();
+}
+
+// transfer "returnTo" value from session to expressJS locals object
+module.exports.storeReturnTo = (req, res, next) => {
+    if(req.session.returnTo){
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next()
 }
