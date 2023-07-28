8762d17d768c65610f323e8cdbd31605026d3531
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-28 01:38:45 -0400

    adding logout route

--- routes/users.js
+++ routes/users.js
@@ -31,5 +31,16 @@ router.post('/login', passport.authenticate('local', {failureFlash: true, failur
     res.redirect('/campgrounds')
 })
 
+router.get('/logout', (req, res, next) => {
+    // req.logout requires a callback to handle errors
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
+
 
 module.exports = router
