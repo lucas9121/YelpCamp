181cf495ca4c7729290bc9f5a59151916bc03b66
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-07-29 02:15:01 -0400

    adding middlewares

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Campgound = require('./models/campground')
+const Review = require('./models/review')
+const {campgroundSchema, reviewSchema} = require('./schemas')
+const ExpressError = require('./utils/ExpressError')
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl
@@ -13,4 +18,37 @@ module.exports.storeReturnTo = (req, res, next) => {
         res.locals.returnTo = req.session.returnTo;
     }
     next()
+}
+
+// joi validation for campground
+module.exports.validateCampground = (req, res, next) => {
+    // server side validation
+    const {error} = campgroundSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
+module.exports.isAuthor = async(req, res, next) => {
+    const {id} = req.params
+    const campground = await Campground.findById(id)
+    if(!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permision to do that!')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+// joi validation for review
+module.exports.validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
 }
