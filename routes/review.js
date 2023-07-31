7d3e2487f41aa5742c3b7cca0220019e6f106624
Lucas Carlos <lucas2carlos@gmail.com>
2023-07-31 14:12:34 -0400

    refactoring review routes

--- routes/review.js
+++ routes/review.js
@@ -1,34 +1,17 @@
 const express = require('express')
 // brings params from the app file to the router
 const router = express.Router({mergeParams: true})
-const Review = require('../models/review')
-const Campground = require('../models/campground')
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
+const reviews = require('../controllers/review')
 const catchAsync = require('../utils/catchAsync')
 
 
 
 
-/////////////// CRUD operations ////////////////
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    review.author = req.user._id;
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    req.flash('success', 'Created new review!')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+/////////////// CRUD operations routes////////////////
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.create))
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const {id, reviewId} = req.params
-    //$pull operator removes from an existing array all instances of a value that match the specified condition.
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    req.flash('success', 'Review sucessfully deleted.')
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.Delete))
 
 
 
