c7c92d19f1e843eea261644953e830240b8cd4ef
Lucas Carlos <lucas2carlos@gmail.com>
2023-08-01 12:20:58 -0400

    grouping same paths

--- routes/campground.js
+++ routes/campground.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const Campground = require('../models/campground')
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware')
 const campgrounds = require('../controllers/campground')
 
@@ -8,18 +7,18 @@ const catchAsync = require('../utils/catchAsync')
 
 
 /////////////// CRUD operations routes ////////////////
-router.get('/', campgrounds.index)
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.create))
 
 router.get('/new', isLoggedIn, campgrounds.newForm)
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.create))
-
-router.get('/:id', catchAsync(campgrounds.show))
+router.route('/:id')
+    .get(catchAsync(campgrounds.show))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.update))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.Delete))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.edit))
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.update))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.Delete))
 
 module.exports = router
