5cd7a213f9ac671033a9239933e1ecb4dcee86bb
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-08-07 21:20:35 -0400

    adding images

--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -42,3 +42,17 @@ module.exports.places = [
     'Cliffs',
     'Hollow'
 ]
+
+module.exports.images = [
+    'https://images.unsplash.com/photo-1430000589629-f04107b5597c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0NDgwNTE2fHx8fHx8fDE2OTEzNjY1Mzc&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1527348550909-8b0444d2b5b3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0NDgwNTE2fHx8fHx8fDE2OTEzNjY2Mzc&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1515496331210-b19007bae145?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0NDgwNTE2fHx8fHx8fDE2OTEzNjY2NjM&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1478281437946-5179e2443a10?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0NDgwNTE2fHx8fHx8fDE2OTEzNjY2ODc&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1437382586735-b2af91b541a9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0NDgwNTE2fHx8fHx8fDE2OTEzNjY3MTc&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    
+    'https://images.unsplash.com/photo-1428532028020-28291ab4cf80?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY5MTM2Njc1OQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1477512076069-d31eb021716f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY5MTM2Njc3OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/reserve/OQx70jjBSLOMI5ackhxm_urbex-ppc-030.jpg?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY5MTM2NjgxNQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1507371341162-763b5e419408?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY5MTM2Njk2Ng&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+    'https://images.unsplash.com/photo-1497303742222-d5f5b02a68be?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY5MTM2NjgzNA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080'
+]
