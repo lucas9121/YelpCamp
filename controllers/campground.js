ddf998d363ca418ca82b37e83c7de9b6e00eda49
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-08-14 00:02:38 -0400

    adding geocoding

--- controllers/campground.js
+++ controllers/campground.js
@@ -1,4 +1,13 @@
 const Campground = require('../models/campground')
+
+// require geocoding service from mapbox
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+// my token
+const mapboxToken = process.env.MAPBOX_TOKEN
+// instantiate a new mapbox geocoding instance pass my token
+const geocoder = mbxGeocoding({accessToken: mapboxToken})
+
+
 const {cloudinary} = require('../cloudinary')
 
 module.exports = {index, newForm, create, show, edit, update, Delete}
@@ -17,14 +26,21 @@ function newForm(req, res) {
 
 // Create
 async function create(req, res, next) {
-    // if(!req.body.campgground) throw new ExpressError('Invalid Campground Data', 400)
-    const newCampground = new Campground(req.body.campground)
-    newCampground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    newCampground.author = req.user._id
-    await newCampground.save()
-    //dismissible message
-    req.flash('success', 'Successfully made a new campground')
-    res.redirect(`/campgrounds/${newCampground._id}`)
+    // geocode map location
+    // https://github.com/mapbox/mapbox-sdk-js/blob/d8ce98bda4c26cef86a8af9132de904de20ed71b/docs/services.md#forwardgeocode
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    console.log(geoData.body.features[0].geometry.coordinates)
+    res.send('OK!!!!')
+    // const newCampground = new Campground(req.body.campground)
+    // newCampground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    // newCampground.author = req.user._id
+    // await newCampground.save()
+    // //dismissible message
+    // req.flash('success', 'Successfully made a new campground')
+    // res.redirect(`/campgrounds/${newCampground._id}`)
 }
 
 // Show
