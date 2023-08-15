874af2106b74122fd1be7b0f72172045f98b523c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-08-15 01:51:58 -0400

    adding marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,12 @@
+
 mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: (coordinates), // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
+
+const marker = new mapboxgl.Marker()
+    .setLngLat(coordinates)
+    .addTo(map)
