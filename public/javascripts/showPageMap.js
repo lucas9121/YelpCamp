777a311fd720cfb9599a2b24f549f8c611ffa7a3
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-08-17 15:41:33 -0400

    changing zoom

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: (coordinates), // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    zoom: 13, // starting zoom
 });
 
 const marker = new mapboxgl.Marker()
