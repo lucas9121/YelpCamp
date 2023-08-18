06193306046e33d436bc53ca5f27c93d5d5ea37e
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-08-18 16:31:18 -0400

    adding marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,6 +7,22 @@ const map = new mapboxgl.Map({
     zoom: 13, // starting zoom
 });
 
-const marker = new mapboxgl.Marker()
+new mapboxgl.Marker()
+    // adds ,arker to map
     .setLngLat(coordinates)
+    // when user clicks on marker
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `
+            <h5 style="margin: 0.3rem auto 0 auto">${campgroundTitle}</h5>
+            <ul style= "list-style-type: none; padding: 0">
+                <li>${coordinates[1]},</li>
+                <li>${coordinates[0]}</li>
+            </ul>
+            `
+        )
+    )
+    // add map
     .addTo(map)
+
