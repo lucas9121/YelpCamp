4254de7f01e668874a512b9d27331fd6179a3ca0
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-10-23 22:24:15 -0400

    fixing app

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const LocalStrategy = require('passport-local')
 const mongoSanitize = require('express-mongo-sanitize');
 const User = require('./models/user')
 const helmet = require('helmet')
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 
 const campgroundRoute = require('./routes/campground')
